77a9e9044810dd0c172021321a382074c22e8c7c
Pavel Fokin <fokinpavel2007@outlook.com>
2021-09-09 08:45:40 +0300

    first commit

---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,41 @@
+﻿import destructuring from '../app.js';
+
+test('destructuring1', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        // <- обратите внимание, описание "засекречено"
+      },
+    ],
+  };
+  const result = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: "Описание недоступно"
+    },
+  ];
+  expect(destructuring(character)).toEqual(result);
+});
---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+﻿export default function destructuring(obj) {
+
+    const arrDestr = [];
+
+    obj.special.forEach((item) => {
+        const {
+            id, name, icon, description = 'Описание недоступно',
+        } = item;
+        const arr = {};
+        arr.id = id;
+        arr.name = name;
+        arr.icon = icon;
+        arr.description = description;
+        arrDestr.push(arr);
+    });
+    return arrDestr;
+}
